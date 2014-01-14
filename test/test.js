5c9119368ae60d26978f5768d88d42a51215d775
Philip Roberts <phil@andyet.net>
2014-01-14 10:42:14 +0000

    Add depth and breadth first traversals

--- test/test.js
+++ test/test.js
@@ -1,18 +1,62 @@
 var Tree = require('../tree');
 var level = require('level');
-var db = level(__dirname + '/db-test.db', { valueEncoding: 'json'});
+var leveldown = require('leveldown');
 
-new Tree({
-    db: db,
-    treeId: 'myTree'
-}, function (err, tree) {
-    if (err) throw err;
+var testFile = __dirname + '/db-test.db';
 
-    tree.insertNode('myTree/node-2fea7b89-c1f0-472d-ad16-ff45c24ed698', { foo: 'bar' }, function (err) {
-        if (err) console.log(err);
-        tree.breadthWalk(function (id, children, data) {
-            console.log(id, children, data);
+function test() {
+    var db = level(testFile, { valueEncoding: 'json'});
+
+    new Tree({
+        db: db,
+        treeId: 'myTree'
+    }, function (err, tree) {
+        if (err) throw err;
+        var rootId = tree.rootId();
+        var breadthExpected = 'abcdefghijkl';
+        var breadthActual = '';
+
+        var depthExpected = 'abeijfcdgklh';
+        var depthActual = '';
+
+        tree.setNodeData(rootId, { name: 'a' }, function (err) {
+            tree.insertNode(rootId, { name: 'b' }, function (err, bId) {
+                tree.insertNode(rootId, { name: 'c' }, function (err, cId) {
+                    tree.insertNode(rootId, { name: 'd' }, function (err, dId) {
+                        tree.insertNode(bId, { name: 'e' }, function (err, eId) {
+                            tree.insertNode(bId, { name: 'f' }, function (err, fId) {
+                                tree.insertNode(dId, { name: 'g' }, function (err, gId) {
+                                    tree.insertNode(dId, { name: 'h' }, function (err, hId) {
+                                        tree.insertNode(eId, { name: 'i' }, function (err, iId) {
+                                            tree.insertNode(eId, { name: 'j' }, function (err, jId) {
+                                                tree.insertNode(gId, { name: 'k' }, function (err, kId) {
+                                                    tree.insertNode(gId, { name: 'l' }, function (err, lId) {
+
+                                                        console.log('Breadth:');
+                                                        tree.breadthWalk(function (id, children, data) {
+                                                            breadthActual += data.name;
+                                                        }, function () {
+                                                            console.log(breadthExpected === breadthActual, breadthActual);
+                                                        });
+
+                                                        tree.depthWalk(function (id, children, data) {
+                                                            depthActual += data.name;
+                                                        }, function () {
+                                                            console.log(depthExpected === depthActual, depthActual);
+                                                        });
+                                                    });
+                                                });
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
         });
     });
-});
+}
 
+leveldown.destroy(testFile, test);
--- tree.js
+++ tree.js
@@ -23,13 +23,16 @@ Tree.prototype.makeNodeDataId = function (id) {
     return id + '/data';
 };
 
-
 Tree.prototype.rootId = function () {
     return this.treeId + '/root';
 };
 
 Tree.prototype.makeRoot = function (cb) {
     this.db.put(this.rootId(), [], cb);
+};
+
+Tree.prototype.setNodeData = function (id, data, cb) {
+    this.db.put(this.makeNodeDataId(id), data, cb);
 };
 
 Tree.prototype.getNodeData = function (id, cb) {
@@ -40,26 +43,62 @@ Tree.prototype.getNode = function (id, cb) {
     this.db.get(id, cb);
 };
 
+Tree.prototype.getNodeAndData = function (id, cb) {
+    this.getNode(id, function (err, children) {
+        if (err) return cb(err);
+        this.getNodeData(id, function (err, data) {
+            cb(err, children, data);
+        }.bind(this));
+    }.bind(this));
+};
+
 Tree.prototype.getRoot = function (cb) {
     this.getNode(this.rootId(), cb);
 };
 
-Tree.prototype.breadthWalk = function (onNode) {
+Tree.prototype.breadthWalk = function (onNode, done) {
     var id = this.rootId();
+    var queue = [];
+    queue.push(id);
 
-    var visitNode = function (id, onNode) {
-        this.getNode(id, function (err, children) {
-            this.getNodeData(id, function (err, data) {
-                onNode(id, children, data);
-                children.forEach(function(child) {
-                    visitNode(child, onNode);
-                }.bind(this));
-            }.bind(this));
-        }.bind(this));
+    var workQueue = function () {
+        var next = queue.shift();
+        
+        this.getNodeAndData(next, function (err, children, data) {
+            onNode(next, children, data);
+            queue.push.apply(queue, children);
+            if (queue.length) {
+                workQueue();
+            } else {
+                done();
+            }
+        });
+    }.bind(this);
+    workQueue();
+};
+
+Tree.prototype.depthWalk = function (onNode, done) {
+    var id = this.rootId();
+    var stack = [];
+    stack.push(id);
+
+    var workStack = function () {
+        var next = stack.pop();
+        
+        this.getNodeAndData(next, function (err, children, data) {
+            onNode(next, children, data);
+            stack.push.apply(stack, children.reverse());
+            if (stack.length) {
+                workStack();
+            } else {
+                done();
+            }
+        });
     }.bind(this);
 
-    visitNode(id, onNode);
+    workStack();
 };
+
 
 Tree.prototype.insertNode = function (parentId, data, cb) {
     var id = this.makeNodeId();
@@ -71,6 +110,9 @@ Tree.prototype.insertNode = function (parentId, data, cb) {
                 .put(parentId, parent)
                 .put(id, [])
                 .put(dataId, data)
-                .write(cb);
+                .write(function (err) {
+                    if (err) return cb(err);
+                    else return cb(null, id);
+                });
     }.bind(this));
 };
